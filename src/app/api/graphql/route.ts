b5441bc5cc11971156974a2fc164244629efbc90
Grant Barchard <grant.barchard@gmail.com>
2024-01-16 21:56:08 -0500

    graphql server

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,18 @@
+import { startServerAndCreateNextHandler } from '@as-integrations/next'
+import { ApolloServer } from '@apollo/server'
+import typeDefs from './schema/schema.graphql'
+
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+}
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+})
+
+const handler = startServerAndCreateNextHandler(server)
+
+export { handler as GET, handler as POST }
